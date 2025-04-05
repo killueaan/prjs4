e29e0d66f9509a5a2270e10e1561296acf186c2e
killueaan <clishkomlenivaya@gmail.com>
2025-04-05 11:53:51 +0300

    практика №4 по js

--- js/script2.js
+++ js/script2.js
@@ -1,39 +1,39 @@
 "use strict";
 
 // //1
-// let a = +prompt('введите a');
-// let b = +prompt('введите b');
-// let start = (a < b) ? a : b;
-// let end = (a < b) ? b : a;
-// for (let i = start; i <= end; i++) {
-//     (i % 2 == 0) ? alert(i) : '';
-// }
+let a = +prompt('введите a');
+let b = +prompt('введите b');
+let start = (a < b) ? a : b;
+let end = (a < b) ? b : a;
+for (let i = start; i <= end; i++) {
+    (i % 2 == 0) ? alert(i) : '';
+}
 
 // //2
-// let i = 0;
-// while (i < 3) {
-//     alert(`number ${i}!`);
-//     i++;
-// }
+let i = 0;
+while (i < 3) {
+    alert(`number ${i}!`);
+    i++;
+}
 
 // //3
-// while (true) {
-//     let a = +prompt('введите число, большее 10');
-//     if (a > 10 || !a) break;
-// }
+while (true) {
+    let a = +prompt('введите число, большее 10');
+    if (a > 10 || !a) break;
+}
 
 // 4
-// let a = +prompt('введите a');
-// let b = +prompt('введите b');
-// function min(a, b) { (a < b) ? alert(a) : alert(b) };
-// min(a, b);
+let c = +prompt('введите a');
+let d = +prompt('введите b');
+function min(c, d) { (c < d) ? alert(c) : alert(d) };
+min(c, d);
 
 
 // //5
-// let ask = (question, yes, no) => { (confirm(question)) ? yes() : no(); }
-// ask('Вы согласны?',
-//     () => alert('вы согласились'),
-//     () => alert('вы отменили выполнение'));
+let ask = (question, yes, no) => { (confirm(question)) ? yes() : no(); }
+ask('Вы согласны?',
+    () => alert('вы согласились'),
+    () => alert('вы отменили выполнение'));
 
 //6
 let age = +prompt('введите возраст', '18');
